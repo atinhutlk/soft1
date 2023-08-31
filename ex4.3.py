759acf7dc7fd534d65525fbf681e299f46fa3275
atinhutlk <nhut.vo@metropolia.fi>
2023-08-31 11:02:53 +0300

    updated

--- ex4.3.py
+++ ex4.3.py
@@ -1,15 +1,18 @@
 """Write a program that asks the user to enter numbers until they enter an empty string to quit.
  Finally, the program prints out the smallest and largest number from the numbers it received.
  """
-smallest = None
-largest = None
-while True:
-    number = input('Enter number: ')
-    if number == "":
-        break
-    if smallest is None or number < smallest:
-        smallest = number
-    if largest is None or number > largest:
-        largest = number
-print(f'The smallest number is {smallest}')
-print(f'The largest number is {largest}')
+user_input = input('Enter a number: ')
+if user_input != "":
+    smallest = None
+    largest = None
+    while user_input != "":
+        number = int(user_input)
+        if smallest is None or number < smallest:
+            smallest = number
+        if largest is None or number > largest:
+            largest = number
+        user_input = input('Enter a number : ')
+    print(f'The smallest number is {smallest}')
+    print(f'The largest number is {largest}')
+else:
+    print('No numbers were given')
