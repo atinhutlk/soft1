1c3d93202b9c07260b242b36253b065f54f312bc
atinhutlk <nhut.vo@metropolia.fi>
2023-09-06 12:10:01 +0300

    updated

--- ex5.2.py
+++ ex5.2.py
@@ -1,3 +1,19 @@
 """Write a program that asks the user to enter numbers until they input an empty string to quit.
 At the end, the program prints out the five greatest numbers sorted in descending order.
 Hint: You can reverse the order of sorted list items by using the sort method with the reverse=True argument."""
+
+arr = []
+
+while True:
+    number = input('Enter number (or press Enter to quit): ')
+
+    if number == '':
+        break
+    else:
+        number = int(number)
+        arr.append(number)
+arr.sort(reverse=True)
+
+print('The five greatest numbers are:')
+for i in range(0, 5):
+    print(arr[i])
---
+++ ex5.3.py
@@ -0,0 +1,15 @@
+"""Write a program that asks the user for an integer and tells if the number is a prime number.
+Prime numbers are number that are only divisible by one or the number itself.
+
+For example, 13 is a prime number as it can only be divided by 1 or 13 so that the result is an integer.
+On the other hand, 21 is not a prime number as it is divisible by 3 and 7."""
+
+number = int(input('Enter the number: '))
+count = 0
+for i in range(1, number+1):
+    if number % i == 0:
+        count += 1
+if count == 2:
+    print(f'{number} is a Prime number')
+else:
+    print(f'{number} is not a Prime number')
